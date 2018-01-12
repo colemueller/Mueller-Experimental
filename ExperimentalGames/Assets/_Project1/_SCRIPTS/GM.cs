e3545e75e740403713dc4f03fd19355efbce54a2
Cole <coleaaronmueller@gmail.com>
2018-01-12 12:31:12 -0500

    Set up scene, got screen darkening

---
+++ ExperimentalGames/Assets/_Project1/_SCRIPTS/GM.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM : MonoBehaviour {
+
+    public bool isPlayerOneTurn = true;
+    public GameObject darken;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isPlayerOneTurn)
+        {
+            darken.transform.position = new Vector3(4.5f, 0, -1);
+        }
+        else
+        {
+            darken.transform.position = new Vector3(-4.5f, 0, -1);
+        }
+	}
+
+    public void DoTurn()
+    {
+        isPlayerOneTurn = !isPlayerOneTurn;
+    }
+}
