b0954c7d60f81cc59a9b0dc3d1091047221cec54
Andrew <adb96@drexel.edu>
2018-02-08 17:08:27 -0500

    Made the project3 files

---
+++ ExperimentalGames/Assets/_Project3/_SCRIPTS/mover.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mover : MonoBehaviour {
+
+	public GameObject leftLeg;
+	public GameObject rightLeg;
+
+	private int screenSizeX;
+
+	// Use this for initialization
+	void Start () {
+		leftLeg.transform.rotation = new Quaternion (0,0,0,0);
+		rightLeg.transform.rotation = new Quaternion (0,0,0,0);
+		screenSizeX = Screen.width;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float screenMid = screenSizeX / 2;
+
+		if (Input.mousePosition.x <= screenMid) {
+			this.transform.RotateAround (rightLeg.transform.position, Vector3.forward, 0f);
+			if (this.transform.rotation == new Quaternion (0,0,0,1)) {
+				this.transform.RotateAround (leftLeg.transform.position, Vector3.forward, 5f);
+			}
+
+		} else if (Input.mousePosition.x >= screenMid) {
+			this.transform.RotateAround (leftLeg.transform.position, Vector3.forward, 0f);
+			if (this.transform.rotation == new Quaternion (0, 0, 0, 1)) {
+				this.transform.RotateAround (rightLeg.transform.position, Vector3.forward, -5f);
+			}
+
+		}
+		
+		
+	}
+}
