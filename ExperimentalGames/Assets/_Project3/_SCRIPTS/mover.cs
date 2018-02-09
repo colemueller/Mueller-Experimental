bb51359786a2dd9c234e0c6c3e24f5a2523248bd
Andrew <adb96@drexel.edu>
2018-02-08 19:37:20 -0500

    Got camera left2right movement done

--- ExperimentalGames/Assets/_Project3/_SCRIPTS/mover.cs
+++ ExperimentalGames/Assets/_Project3/_SCRIPTS/mover.cs
@@ -6,32 +6,66 @@ public class mover : MonoBehaviour {
 
 	public GameObject leftLeg;
 	public GameObject rightLeg;
+	public float speed;
 
 	private int screenSizeX;
+	private bool movingLeft;
+	private bool prevDirection;
+	private bool fell;
 
 	// Use this for initialization
 	void Start () {
-		leftLeg.transform.rotation = new Quaternion (0,0,0,0);
-		rightLeg.transform.rotation = new Quaternion (0,0,0,0);
 		screenSizeX = Screen.width;
+		fell = false;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//Find the middle of current screen size
 		float screenMid = screenSizeX / 2;
 
-		if (Input.mousePosition.x <= screenMid) {
-			this.transform.RotateAround (rightLeg.transform.position, Vector3.forward, 0f);
-			if (this.transform.rotation == new Quaternion (0,0,0,1)) {
-				this.transform.RotateAround (leftLeg.transform.position, Vector3.forward, 5f);
+		//If mouse is on screen left
+		if (Input.mousePosition.x < screenMid) {
+			movingLeft = true;
+
+			//Rotate from the right pivot until left pivot is on the ground
+			if (this.transform.rotation.z < -.01f) {
+				this.transform.RotateAround (rightLeg.transform.position, Vector3.forward, speed);
+			} else {
+				//If you fall
+				if (this.transform.rotation.z >= .75f) {
+					fell = true;
+					speed = 0;
+				}
+				this.transform.RotateAround (leftLeg.transform.position, Vector3.forward, speed);
+			}
+		
+		//Else, the mouse is on screen right
+		} else {
+			movingLeft = false;
+
+			//Rotate from the left pivot until the right pivot is on the ground
+			if (this.transform.rotation.z > .01f) {
+				this.transform.RotateAround (leftLeg.transform.position, (Vector3.forward * -1), speed);
+			} else {
+				//If you fall
+				if (this.transform.rotation.z <= -.75f) {
+					fell = true;
+					speed = 0;
+				}
+				this.transform.RotateAround (rightLeg.transform.position, (Vector3.forward * -1), speed);
 			}
 
-		} else if (Input.mousePosition.x >= screenMid) {
-			this.transform.RotateAround (leftLeg.transform.position, Vector3.forward, 0f);
-			if (this.transform.rotation == new Quaternion (0, 0, 0, 1)) {
-				this.transform.RotateAround (rightLeg.transform.position, Vector3.forward, -5f);
-			}
+		}
 
+		//Can't add speed if you've fallen
+		if (!fell) {
+			//Add to speed everytime you change directions
+			if (prevDirection != movingLeft) {
+				speed += .25f;
+				prevDirection = movingLeft;
+			}
 		}
 		
 		
